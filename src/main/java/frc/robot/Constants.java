c67a2a553e36e2ed2051dfc92e1fb5660e9f32a6
Adam Vander Pas <azvanderpas@gmail.com>
2024-12-07 14:50:29 -0600

    Add LowRider Arm Constants using Units library Remove BuildConstants.java from git tracking

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.*;
+
+public final class Constants {
+
+  public static class TrapElvConstants {
+    // Wrist
+    public static final int WRIST_MOTOR_ID = 12;
+    public static final int WRIST_ENCODER_ID = 25;
+
+    public static final Angle WRIST_MIN_ANGLE = Degrees.of(-90);
+    public static final Angle WRIST_MAX_ANGLE = Degrees.of(270);
+    public static final Distance WRIST_LENGTH = Inches.of(11.877934);
+    public static final MomentOfInertia WRIST_MOI = KilogramSquareMeters.of(0.3175242664);
+    public static final double WRIST_GEAR_RATIO = 35;
+    public static final Angle WRIST_ZERO_OFFSET = Revolutions.of(0.8164931);
+  }
+}
