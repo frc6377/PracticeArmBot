e45efb2506caa81111ae2bdb5e0a38109bb3bfd9
jameson-pi <piscitello284@gmail.com>
2024-12-14 15:38:50 -0600

    make an arm in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import frc.robot.subsystem.arm;
 public class RobotContainer {
+  private final arm armSubsystem;
   public RobotContainer() {
     configureBindings();
+    armSubsystem = new arm();
   }
 
   private void configureBindings() {}
