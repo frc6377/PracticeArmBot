fa60825c74404d67346559d2d7c864b81aae77d6
jameson-pi <piscitello284@gmail.com>
2024-12-18 20:34:52 -0600

    make AI work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,24 +1,31 @@
+package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.arm;
 import frc.robot.commands.SetArmPositionCommand;
+import frc.robot.subsystem.RobotArm;
 
 public class RobotContainer {
-    private final Subsystem robotArm = new arm();
-    private final CommandXboxController controller = new CommandXboxController(0);
-    
-    public RobotContainer() {
-        configureBindings();
-    }
-    
-    private void configureBindings() {
-        // Move arm to 45 degrees when A button is pressed
-        controller.a().onTrue(new SetArmPositionCommand(robotArm, 45.0));
-        
-        // Move arm to -45 degrees when B button is pressed
-        controller.b().onTrue(new SetArmPositionCommand(robotArm, -45.0));
-        
-        // Return to 0 degrees when X button is pressed
-        controller.x().onTrue(new SetArmPositionCommand(robotArm, 0.0));
-    }
+  private final RobotArm robotArm = new RobotArm();
+  private final CommandXboxController controller = new CommandXboxController(0);
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    // Move arm to 45 degrees when A button is pressed
+    controller.a().onTrue(new SetArmPositionCommand(robotArm, 45.0));
+
+    // Move arm to -45 degrees when B button is pressed
+    controller.b().onTrue(new SetArmPositionCommand(robotArm, -45.0));
+
+    // Return to 0 degrees when X button is pressed
+    controller.x().onTrue(new SetArmPositionCommand(robotArm, 0.0));
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
 }
